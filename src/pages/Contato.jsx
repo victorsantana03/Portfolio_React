a796776112d037d18552908de6acac5546e177dc
victorsantana03 <viclucigei.123@gmail.com>
2025-01-13 14:54:23 -0300

    alterando curriculo

--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -1,7 +1,7 @@
 import { Header } from "../components/Header"
 import { FaDownload } from "react-icons/fa";
 
-import pdf from "../assets/documents/curriculo-victor-2024.pdf"
+import pdf from "../assets/documents/Curriculo_Victor - 2024.pdf"
 
 export function Contato(){
     return (
