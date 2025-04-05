fb34a2a17df35307e9a4f5d924c3603c0ff860db
victorsantana03 <viclucigei.123@gmail.com>
2025-04-05 16:32:10 -0300

    atualizando portfólio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,56 @@
 import { Header } from "../components/Header";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
-import imgPerfil from "../assets/images/home/perfil-portfolio.jpg"
-
+import imgPerfil from "../assets/images/home/perfil-portfolio.jpg";
 
 export function Home() {
-    return (
-        <div>
-            <section className="flex flex-col h-screen">
-                <Header />
+  return (
+    <div>
+      <section className="flex flex-col h-screen">
+        <Header />
 
-                <main className="lg:h-full flex justify-center items-center bg-background ">
-                    <div className="flex flex-col lg:flex-row mx-5 lg:mx-0 my-20 lg:my-0 w-full items-center justify-around gap-20 lg:gap-0">
-                        <div className="flex flex-col gap-3 font-poppins max-w-2xl">
-                            <h2 className="text-secondary text-3xl lg:text-5xl font-orbitron">// Front-End Developer</h2>
-                            <h1 className="text-2xl lg:text-4xl font-semibold text-white">Victor Alves Santana</h1>
-                            <p className="text-lg lg:text-xl text-white">Olá. Bem-vindo ao meu portfólio! Fique a vontade para me conhecer melhor.</p>
-                            <div className="flex items-center gap-2">
-                                <p className="font-bold text-white">Minhas redes:</p>
-                                <a href="https://www.linkedin.com/in/victor-alves-santana-111440196/" target="_blank" rel="noopener noreferrer"><FaLinkedin className="text-secondary text-3xl cursor-pointer hover:-translate-y-1 hover:scale-100 duration-300" /></a>
-                                <a href="https://github.com/victorsantana03" target="_blank" rel="noopener noreferrer"><FaGithub className="text-secondary text-3xl cursor-pointer hover:-translate-y-1 hover:scale-100 duration-300" /></a>
-                            </div>
-                        </div>
+        <main className="lg:h-full flex justify-center items-center bg-background ">
+          <div className="flex flex-col lg:flex-row mx-5 lg:mx-0 my-20 lg:my-0 w-full items-center justify-around gap-20 lg:gap-0">
+            <div className="flex flex-col gap-3 font-poppins max-w-2xl">
+              <h2 className="text-secondary text-3xl lg:text-5xl font-orbitron">
+                // Full-Stack Developer
+              </h2>
+              <h1 className="text-2xl lg:text-4xl font-semibold text-white">
+                Victor Alves Santana
+              </h1>
+              <p className="text-lg lg:text-xl text-white">
+                Olá. Bem-vindo ao meu portfólio! Fique a vontade para me
+                conhecer melhor.
+              </p>
+              <div className="flex items-center gap-2">
+                <p className="font-bold text-white">Minhas redes:</p>
+                <a
+                  href="https://www.linkedin.com/in/victor-alves-santana-111440196/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaLinkedin className="text-secondary text-3xl cursor-pointer hover:-translate-y-1 hover:scale-100 duration-300" />
+                </a>
+                <a
+                  href="https://github.com/victorsantana03"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaGithub className="text-secondary text-3xl cursor-pointer hover:-translate-y-1 hover:scale-100 duration-300" />
+                </a>
+              </div>
+            </div>
 
-                        <div className="flex z-10">
-                            <img src={imgPerfil} alt="" className="size-72 lg:size-80 xl:size-96 object-cover rounded-full shadow-2xl shadow-border_color animate-shake animate-infinite animate-duration-[8000ms] animate-ease-linear" />
-                        </div>
-                    </div>
-
-                </main>
-            </section>
-        </div>
-    )
+            <div className="flex z-10">
+              <img
+                src={imgPerfil}
+                alt=""
+                className="size-72 lg:size-80 xl:size-96 object-cover rounded-full shadow-2xl shadow-border_color animate-shake animate-infinite animate-duration-[8000ms] animate-ease-linear"
+              />
+            </div>
+          </div>
+        </main>
+      </section>
+    </div>
+  );
 }
--- src/pages/Projetos.jsx
+++ src/pages/Projetos.jsx
@@ -1,78 +1,84 @@
-import { Header } from "../components/Header"
+import { Header } from "../components/Header";
 import Projeto from "../components/Projeto";
 
-
 export function Projetos() {
-    return (
-        <section className="flex flex-col  min-h-0">
-            <Header />
-            <main className="h-full flex flex-col justify-center items-center bg-background font-poppins">
+  return (
+    <section className="flex flex-col  min-h-0">
+      <Header />
+      <main className="h-full flex flex-col justify-center items-center bg-background font-poppins">
+        <div className="flex justify-around items-center w-full my-20">
+          <h1 className="text-white text-3xl lg:text-4xl font-bold font-orbitron">
+            // Últimos <span className="text-secondary">Projetos</span>
+          </h1>
+          <p className="text-gray-300 font-medium text-lg animate-pulse hidden md:flex">
+            Confira alguns dos meus projetos
+          </p>
+        </div>
 
-                <div className="flex justify-around items-center w-full my-20">
-                    <h1 className="text-white text-3xl lg:text-4xl font-bold font-orbitron">// Últimos <span className="text-secondary">Projetos</span></h1>
-                    <p className="text-gray-300 font-medium text-lg animate-pulse hidden md:flex">Confira alguns dos meus projetos</p>
-                </div>
-                
-                <div className="grid grid-cols-1 gap-20 2xl:grid-cols-3 xl:grid-cols-2 2xl:gap-36 2xl:gap-y-24 mb-14">
-                    
-                    <Projeto 
-                     imgUrl="/image-projects/Projeto_Maiz.png" 
-                     title="Maiz - Comida Árabe" 
-                     description="Uma ideia de e-commerce envolvendo um restaurante de comida árabe. O site contém um sistema de carrinho que permite o usuário adicionar o pedido, ver a quantidade e descartar se necessário. Totalmente responsivo." 
-                     tecs={["HTML", "Tailwind", "JavaScript"]} 
-                     linkDeploy="https://victorsantana03.github.io/maiz_ecommerce/"
-                     linkGH="https://github.com/victorsantana03/maiz_ecommerce" 
-                    />
+        <div className="grid grid-cols-1 gap-20 2xl:grid-cols-3 xl:grid-cols-2 2xl:gap-36 2xl:gap-y-24 mb-14">
+          <Projeto
+            imgUrl="/image-projects/Projeto_Brastecno.png"
+            title="Brastecno Landing Page"
+            description="Meu primeiro projeto real feito para uma empresa de assistência de eletrodomésticos. Uma landing page feito em dupla utilizando git para versionamento do código. Totalmente responsivo e estilizado em Tailwind."
+            tecs={["Tailwind", "HTML", "JavaScript"]}
+            linkDeploy="https://www.brastecnoguarulhos.com.br/"
+            linkGH="https://github.com/victorsantana03/Brastecno-LandingPage"
+          />
 
-                    <Projeto 
-                     imgUrl="/image-projects/Projeto_Brastecno.png" 
-                     title="Brastecno Landing Page" 
-                     description="Meu primeiro projeto real feito para uma empresa de assistência de eletrodomésticos. Uma landing page feito em dupla utilizando git para versionamento do código. Totalmente responsivo e estilizado em Tailwind." 
-                     tecs={["Tailwind", "HTML", "JavaScript"]} 
-                     linkDeploy="https://www.brastecnoguarulhos.com.br/"
-                     linkGH="https://github.com/victorsantana03/Brastecno-LandingPage"  
-                    />
+          <Projeto
+            imgUrl="/image-projects/Projeto_Twitter.png"
+            title="Twitter em React"
+            description="Uma aplicação inspirada no Twitter. Nesse projeto foi utilizado API's para gerar fotos de perfis e imagens aleatórias simbolizando posts. Também foi utilizado UseState e UseEffect do React. Os Tweets são gerados por setInterval."
+            tecs={["Vite", "React", "Tailwind"]}
+            linkDeploy="https://victorsantana03.github.io/twitter-react/"
+            linkGH="https://github.com/victorsantana03/twitter-react"
+          />
 
-                    <Projeto 
-                     imgUrl="/image-projects/Projeto_Twitter.png" 
-                     title="Twitter em React" 
-                     description="Uma aplicação inspirada no Twitter. Nesse projeto foi utilizado API's para gerar fotos de perfis e imagens aleatórias simbolizando posts. Também foi utilizado UseState e UseEffect do React. Os Tweets são gerados por setInterval." 
-                     tecs={["Vite", "React", "Tailwind"]} 
-                     linkDeploy="https://victorsantana03.github.io/twitter-react/"
-                     linkGH="https://github.com/victorsantana03/twitter-react"  
-                    />
+          <Projeto
+            imgUrl="/image-projects/Projeto_MultistepForm.png"
+            title="Formulário Multistep"
+            description="Um formulário de avaliação para e-commerce com múltiplas etapas feito em React. Pratiquei conceitos essenciais de React realizando esse projeto, como persistência de dados entre componentes."
+            tecs={["React", "CSS", "JavaScript", "Vite"]}
+            linkDeploy="https://victorsantana03.github.io/multistep-form-react/"
+            linkGH="https://github.com/victorsantana03/multistep-form-react"
+          />
 
-                    <Projeto 
-                     imgUrl="/image-projects/Projeto_Slider.png" 
-                     title="Slider Cereais" 
-                     description="Um projeto mais simples focado em animações CSS. A idéia é um slider em carrosel com imagens de alguns cereais e suas respectivas informações. Um projeto pensado para uma loja cerealista." 
-                     tecs={["HTML", "CSS", "JavaScript"]} 
-                     linkDeploy="https://victorsantana03.github.io/slider/"
-                     linkGH="https://github.com/victorsantana03/slider"  
-                    />
+          <Projeto
+            imgUrl="/image-projects/Projeto_Maiz.png"
+            title="Maiz - Comida Árabe"
+            description="Uma ideia de e-commerce envolvendo um restaurante de comida árabe. O site contém um sistema de carrinho que permite o usuário adicionar o pedido, ver a quantidade e descartar se necessário. Totalmente responsivo."
+            tecs={["HTML", "Tailwind", "JavaScript"]}
+            linkDeploy="https://victorsantana03.github.io/maiz_ecommerce/"
+            linkGH="https://github.com/victorsantana03/maiz_ecommerce"
+          />
 
-                    <Projeto 
-                     imgUrl="/image-projects/Projeto_MultistepForm.png" 
-                     title="Formulário Multistep" 
-                     description="Um formulário de avaliação para e-commerce com múltiplas etapas feito em React. Pratiquei conceitos essenciais de React realizando esse projeto, como persistência de dados entre componentes." 
-                     tecs={["React", "CSS", "JavaScript", "Vite"]} 
-                     linkDeploy="https://victorsantana03.github.io/multistep-form-react/"
-                     linkGH="https://github.com/victorsantana03/multistep-form-react"  
-                    />
+          <Projeto
+            imgUrl="/image-projects/Projeto_Covid.png"
+            title="Estatísticas globais COVID-19"
+            description="Este App consome uma API e exibe um mapa mundi com marcadores em todos os países exibindo estatísticas locais da COVID. Um ótimo projeto pra testar novas bibliotecas."
+            tecs={["React", "Node", "Vite", "Tailwind"]}
+            linkDeploy="https://victorsantana03.github.io/EstatisticasCovid19-React/"
+            linkGH="https://github.com/victorsantana03/EstatisticasCovid19-React"
+          />
 
-                    <Projeto 
-                     imgUrl="/image-projects/Projeto_Spotify.png" 
-                     title="Spotify em React" 
-                     description="Uma cópia do Spotify feita em React utilizando tecnologias importantes dessa biblioteca. Utilizei React-Router-Dom para fazer a navegação entre páginas, Props para passagem de dados, renderização condicional e outros..." 
-                     tecs={["React", "CSS", "JavaScript", "Vite"]} 
-                     linkGH="https://github.com/victorsantana03/Spotify-React"  
-                    />
+          <Projeto
+            imgUrl="/image-projects/Projeto_Spotify.png"
+            title="Spotify em React"
+            description="Uma cópia do Spotify feita em React utilizando tecnologias importantes dessa biblioteca. Utilizei React-Router-Dom para fazer a navegação entre páginas, Props para passagem de dados, renderização condicional e outros..."
+            tecs={["React", "CSS", "JavaScript", "Vite"]}
+            linkGH="https://github.com/victorsantana03/Spotify-React"
+          />
 
-                </div>
-
-
-            </main>
-
-        </section>
-    )
+          <Projeto
+            imgUrl="/image-projects/Projeto_Slider.png"
+            title="Slider Cereais"
+            description="Um projeto mais simples focado em animações CSS. A idéia é um slider em carrosel com imagens de alguns cereais e suas respectivas informações. Um projeto pensado para uma loja cerealista."
+            tecs={["HTML", "CSS", "JavaScript"]}
+            linkDeploy="https://victorsantana03.github.io/slider/"
+            linkGH="https://github.com/victorsantana03/slider"
+          />
+        </div>
+      </main>
+    </section>
+  );
 }
